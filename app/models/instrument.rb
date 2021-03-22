38b831fe41c6ebe618b1c4dd8f732caa4c012de1
Matthew <elijah2895@hotmail.com>
2021-03-22 12:04:06 -0400

    working prgoress

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,3 @@
 class Instrument < ApplicationRecord
+    has_many :artists
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  resources :artists
+  resources :instruments
+
+  #get '/instruments', to: 'instruments#index', as: 'instruments'
+  #get '/dogs', to: 'dogs#index', as: 'dogs'
+  #get '/instruments', to: 'instruments#show'
 end
