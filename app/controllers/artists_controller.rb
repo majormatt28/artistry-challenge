e302bf987f0549b5abfa364b54e7cb59ecebdf2c
Matthew <elijah2895@hotmail.com>
2021-03-22 12:36:31 -0400

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,29 @@ class ArtistsController < ApplicationController
     def show
         @artist = Artist.find(params[:id])
     end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+
+        if @artist.valid?
+            rediect_to artist_path(@artist.id)
+        else
+            flash[:errors] = @snack.errors.full_messages
+            redirect_to artist_path(@artist.id)
+        end
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :age, :title)
+    end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ApplicationRecord
     has_many :instruments
+
+    validates :name, presence: true
+    validates :title, presence: true
 end
